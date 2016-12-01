eb51e74512db5e8d2db44eb69d7dda53c5c70cc2
Dominik Czarnota <dominik.b.czarnota@gmail.com>
2016-12-01 17:06:45 +0100

    day 01: 2nd part done (Python 2/3)

--- day01/main.py
+++ day01/main.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import enum
 import numpy as np
 
+
 d = enum.Enum('Direction', ('N', 'E', 'W', 'S'))
 directions = (d.N, d.E, d.S, d.W)
 
@@ -18,8 +19,9 @@ def taxicab_distance(p1, p2):
 
 
 def solve(movements):
-    end_pos = np.array((0, 0))
+    pos = np.array((0, 0))
     direction = d.N
+    visited = {tuple(pos)}
         
     direction_to_axis_step = {
         d.N: np.array((0, 1)),
@@ -33,14 +35,17 @@ def solve(movements):
         steps = int(movement[1:])
 
         direction = make_turn(direction, turn)
-        end_pos += direction_to_axis_step[direction] * steps
 
-    return taxicab_distance((0, 0), end_pos)
+        for step in range(steps):
+            pos += direction_to_axis_step[direction]
+
+            pt = tuple(pos)
+            if pt in visited:
+                return taxicab_distance((0, 0), pos)
+
+            visited.add(pt)
 
 
-# testcases
-assert solve("R2, L3") == 5
-assert solve("R2, R2, R2") == 2
-assert solve("R5, L5, R5, R3") == 12
+assert solve("R8, R4, R4, R8") == 4
 
 print("Teh solution is", solve("R1, R1, R3, R1, R1, L2, R5, L2, R5, R1, R4, L2, R3, L3, R4, L5, R4, R4, R1, L5, L4, R5, R3, L1, R4, R3, L2, L1, R3, L4, R3, L2, R5, R190, R3, R5, L5, L1, R54, L3, L4, L1, R4, R1, R3, L1, L1, R2, L2, R2, R5, L3, R4, R76, L3, R4, R191, R5, R5, L5, L4, L5, L3, R1, R3, R2, L2, L2, L4, L5, L4, R5, R4, R4, R2, R3, R4, L3, L2, R5, R3, L2, L1, R2, L3, R2, L1, L1, R1, L3, R5, L5, L1, L2, R5, R3, L3, R3, R5, R2, R5, R5, L5, L5, R2, L3, L5, L2, L1, R2, R2, L2, R2, L3, L2, R3, L5, R4, L4, L5, R3, L4, R1, R3, R2, R4, L2, L3, R2, L5, R5, R4, L2, R4, L1, L3, L1, L3, R1, R2, R1, L5, R5, R3, L3, L3, L2, R4, R2, L5, L1, L1, L5, L4, L1, L1, R1"))
